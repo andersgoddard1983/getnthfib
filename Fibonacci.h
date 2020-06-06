2f00238e46c03eb9d6261fc1805c53a7d83c3974
andersgoddard1983 <anders@campaignforalcoholfree.com>
2020-06-06 11:23:25 +0000

    Added the test and code for the Fibonacci sequence, that the next term is equal to the sum of the previous two. Used EXPECT_THAT to check multiple similar cases. All tests pass.

--- Fibonacci.h
+++ Fibonacci.h
@@ -5,9 +5,13 @@ class Fibonacci
 {
  public:
   int nthFib(int n) {
-    if (n == 1) return 0;
-    if (n == 2) return 1;
-    return 0;
+    if (n > 2) {
+      return nthFib(n-1) + nthFib(n-2);
+    } else if (n == 1){
+      return 0;
+    } else {
+    return 1;
+    }
   }
 };
 
--- FibonacciTest.cpp
+++ FibonacciTest.cpp
@@ -17,3 +17,13 @@ TEST_F(FibonacciExample, Returns1ForSecondN) {
   auto result = fibonacci.nthFib(2);
   ASSERT_THAT(result, Eq(1));
 }
+
+TEST_F(FibonacciExample, ReturnsSumOfPreviousTwoNumberes) {
+  EXPECT_THAT(fibonacci.nthFib(3), Eq(1));
+  EXPECT_THAT(fibonacci.nthFib(4), Eq(2));
+  EXPECT_THAT(fibonacci.nthFib(5), Eq(3));
+  EXPECT_THAT(fibonacci.nthFib(6), Eq(5));
+  EXPECT_THAT(fibonacci.nthFib(7), Eq(8));
+  EXPECT_THAT(fibonacci.nthFib(8), Eq(13));
+  EXPECT_THAT(fibonacci.nthFib(9), Eq(21));
+}
