2e00f6e7a8b166edf9b98301f2d2935a3b7e993c
andersgoddard1983 <anders@campaignforalcoholfree.com>
2020-06-06 10:31:37 +0000

    Created tests for n = 1 and 2 and wrote code to make them pass.

--- FibonacciTest.cpp
+++ FibonacciTest.cpp
@@ -1,17 +1,27 @@
 class Fibonacci
 {
 public:
-  int nthfib(int n) {
+  int nthFib(int n) {
+    if (n == 1) return 0;
+    if (n == 2) return 1;
     return 0;
   }
 };
-
 #include "gmock/gmock.h"
+using ::testing::Eq;
 
 TEST(FibonacciExample, Returns0ForFirstN) {
   Fibonacci fibonacci;
 
-  auto result = fibonacci.nthfib(1);
+  auto result = fibonacci.nthFib(1);
 
-  ASSERT_THAT(result, testing::Eq(0));
+  ASSERT_THAT(result, Eq(0));
+}
+
+TEST(FibonacciExample, Returns1ForSecondN) {
+  Fibonacci fibonacci;
+
+  auto result = fibonacci.nthFib(2);
+
+  ASSERT_THAT(result, Eq(1));
 }
